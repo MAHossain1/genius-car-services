cbe8abcea005b663e8542594e1969d45a7eea62f
MAHossain1 <mahpro110@gmail.com>
2022-04-11 12:00:37 +0600

    fake data loaded

--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -1,9 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Service from "../Service/Service";
 
 const Services = () => {
+  const [services, setServices] = useState([]);
+
+  useEffect(() => {
+    fetch("services.json")
+      .then(res => res.json())
+      .then(data => setServices(data));
+  }, []);
   return (
     <div>
-      <h2>Here we are to provide better service</h2>
+      <h3>Services: {services.length}</h3>
+      {services.map(service => (
+        <Service key={service.id} service={service}></Service>
+      ))}
     </div>
   );
 };
